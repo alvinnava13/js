37b24518e6bd501a7ab846d057e0ff9665cf2349
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-17 15:59:21 -0400

    Fix IF statement

--- js2/heehaw.js
+++ js2/heehaw.js
@@ -1,14 +1,14 @@
 for (let i = 1; i <= 100; i++)
 {
-    if((i % 3 == 0) && (i % 5 == 0))
+    if(i % 3 === 0 && i % 5 === 0)
     {
         console.log("Hee Haw!");
     }
-    if(i % 3 == 0)
+    if(i % 3 === 0)
     {
         console.log("Hee!");
     }
-    else if(i % 5 == 0)
+    else if(i % 5 === 0)
     {
         console.log("Haw!");
     }
