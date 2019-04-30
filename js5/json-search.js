abc4f8685bb14e0305fd05be8b865248ab0c14dd
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-29 20:54:00 -0400

    Set ids to variables and predefine a function to show search results

---
+++ js5/json-search.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('search');
+let name = document.getElementById('name');
+let output = document.getElementById('output');
+
+btn.onclick = showResults;
+
+function showResults()
+{
+
+}
