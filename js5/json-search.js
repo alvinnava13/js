5a9023507d4e44156179f5c847606878dc506f82
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-30 20:38:18 -0400

    Try writing a function for searching a name in json file

--- js5/json-search.js
+++ js5/json-search.js
@@ -1,10 +1,26 @@
-let btn = document.getElementById('search');
-let name = document.getElementById('name');
-let output = document.getElementById('output');
+let btn = document.getElementById('search'); // button
+let name = document.getElementById('name'); // textbox
+let output = document.getElementById('output'); // output div
 
 btn.onclick = showResults;
 
 function showResults()
 {
+    $("#search").on('click', function(){
 
+        let name = $("#name").val();
+        let output = $("#output");
+
+        $.getJSON('json-search.json', function(president){
+            output.html("<h3>" + name + "</h3>");
+
+            $.each(president, function(value){
+                if(value.name.toLowerCase().includes(name.toLowerCase()))
+                {
+
+                }
+            });
+
+        });
+    });
 }
