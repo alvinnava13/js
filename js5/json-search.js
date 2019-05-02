2e7b803ad4106127b92375ffcf71d7c1d2dd23ff
Alvin Nava <anava8@mail.greenriver.edu>
2019-05-01 21:55:31 -0400

    Add a function that clears html elements of text if another search was conducted

--- js5/json-search.js
+++ js5/json-search.js
@@ -1,29 +1,50 @@
 let btn = document.getElementById('search'); // button
-let textName = document.getElementById('name'); // textbox
 let output = document.getElementById('output'); // output div
 let info = document.getElementById('info'); // info div
 
+// When button is clicked, invoke search() function
 btn.onclick = search;
 
 
 function search()
 {
+    // Clear the html elements of text if there was a previous search
+    clear();
     let searchName = $("#name").val();
 
+    // Search json file
     $.getJSON("json-search.json", function(result){
         let found = false;
 
+        // For each result returned...
+        // Check to see if there are partial search matches and display full/partial matches
         $.each(result, function(index, item){
-            if(item.name.toLowerCase().includes(searchName.toLowerCase())){
+            if(item.name.toLowerCase().includes(searchName.toLowerCase()))
+            {
                 found = true;
 
+                // Display bio info
                 $("#info").append('<p>Name: ' + item.name + '</p>');
                 $("#info").append('<p>Gender: ' + item.gender + '</p>');
                 $("#info").append('<p>Born: ' + item.born + '</p>');
                 $("#info").append('<p>Died: ' + item.died + '</p>');
                 $("#info").append('<p>Father: ' + item.father + '</p>');
                 $("#info").append('<p>Mother: ' + item.mother + '</p>');
+                $("#info").append('<p>************************</p>');
             }
+
         });
+        // If no results were found, display a message
+        if(found == false)
+        {
+            $("#output").append("<p>No results found.</p>");
+        }
     });
+}
+
+// Function to clear html elements of previous searches
+function clear()
+{
+    $("#info").html("");
+    $("#output").html("");
 }
